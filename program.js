fc87ee185e9c8a54d7f127175a821d5dd324859b
jukmanty <jukka.mantyla@solita.fi>
2015-02-08 22:39:04 +0200

    Output books as JSON and use local MongoDB instance as JSON cache if available

--- program.js
+++ program.js
@@ -1,42 +1,99 @@
 var _ = require('lodash');
-
 var http = require('http');
-var url = 'http://metadata.helmet-kirjasto.fi/search/author.json?query=Campbell';
+var mongodb = require('mongodb');
 
-function createServer(host, port, htmlBody) {
-    var html = "<html><body>" + htmlBody + "</body></html>";
-    http.createServer(function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(html);
-    }).listen(port, host);
-}
+// no need to instatiate multiple servers, configuration included
+var booksServer = (function() {
+    var url = 'http://metadata.helmet-kirjasto.fi/search/author.json?query=Campbell';
+    var host = "localhost";
+    var port = 80;
+    var dbConnectionString = "mongodb://programjs:programjs@localhost:27017/books";
+    var cacheMaxAgeMillis = 86400000; // 1 day
+    var genericAppError = {error: "Application error. Please contact foo.bar@invalid"};
+    var booksDb;
 
-http.get(url, function(res) {
+    var fetchBooksFromLibrary = function(onSuccessCallback) {
+        http.get(url, function(res) {
 
-    var body = "";
-    res.on("data", function(chunk) {
-        body += chunk;
-    });
+            var body = "";
+            res.on("data", function(chunk) {
+                body += chunk;
+            });
 
-    res.on("end", function() {
+            res.on("end", function() {
+                var authorRes = JSON.parse(body);
+                var books = _.map(authorRes.records, function(record) {
+                    return {id: record.library_id, title: record.title, year: record.year}
+                });
+                onSuccessCallback(books);
+            });
 
-        var authorRes = JSON.parse(body);
-
-        var books = _.map(authorRes.records, function(record) {
-            return {displayName: record.title, year: record.year}
+        }).on("error", function(e) {
+              console.log("Error: ", e);
+              onSuccessCallback(genericAppError);
         });
+    };
 
-        var content = "";
+    var getBooks = function(onSuccessCallback) {
+        // db connection established
+        if (booksDb) {
+            var booksCollection = booksDb.collection('books', function(err, collection) {});
+            booksCollection.findOne({}, function(err, booksCacheEntry) {
+                // if there is cache entry and cache entry is fresh enough then return books from cache entry 
+                if (booksCacheEntry && (Date.now() - booksCacheEntry.timestamp < cacheMaxAgeMillis)) {
+                    onSuccessCallback(booksCacheEntry.books);
+                // fetch books from library, cache to db and return books
+                } else if (!err) { 
+                    fetchBooksFromLibrary(function(books) {
+                        storeBooks(books);
+                        // it might make sense to return cache entry while updating books in the background
+                        onSuccessCallback(books); 
+                    });    
+                // log error       
+                } else {
+                    console.log("Error: ", err);
+                    onSuccessCallback(genericAppError);
+                }     
 
-        _.forEach(_.filter(books, function(book) { return book.year >= 2012}), function(book) {
-            content += "<h2>" + book.displayName + "</h2>";
-            content += "<p>" + book.year + "</p>";
-        });
+            });
+        // fetch and return books
+        } else {
+            fetchBooksFromLibrary(onSuccessCallback);
+        }
+    };
 
-        createServer('localhost', 80, content);
+    var storeBooks = function(books) {
+        if (booksDb) {
+            var booksCollection = booksDb.collection('books', function(err, collection) {});
+            booksCollection.update({}, {$set: {type: 'booksCache', timestamp: Date.now(), books: books}}, {upsert: true}, function(err, r) {});
+        }
+    };
 
-    });
+    var start = function () {
+        var mongoClient = mongodb.MongoClient;
+        if (dbConnectionString) {
+                mongoClient.connect(dbConnectionString, function(err, db) {
+                if(!err) {
+                    booksDb = db;
+                }
+            });
+        }
 
-}).on("error", function(e) {
-      console.log("Error: ", e);
-});
+        http.createServer(function(req, res) {
+            if (req.url === '/api/query/books') {
+                getBooks(function(books) {
+                    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+                    res.end(JSON.stringify(books));
+                });
+            } else {
+                res.end();
+            }
+        }).listen(port, host);
+    };
+
+    return {
+        start: start
+    };
+})();
+
+booksServer.start();
